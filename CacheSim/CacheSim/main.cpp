d3bd3d711272bab46983344219bc2f2cd6b61946
Douglas Schiavi <schiavi.douglas@gmail.com>
2019-05-21 18:30:08 -0300

    main.cpp init for mac

---
+++ CacheSim/CacheSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CacheSim
+//
+//  Created by Douglas Schiavi.
+//  Copyright © 2019 Douglas Schiavi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
